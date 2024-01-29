36f2b69f4a3dfd2069ccb58f6b8fc904b1e2259d
Dmitrii Fediuk <dfediuk@gmail.com>
2024-01-29 07:34:30 +0000

    https://github.com/dmitrii-fediuk/5.9.188.84/issues/57

--- var/www/pg/conf/config.inc.php
+++ var/www/pg/conf/config.inc.php
@@ -1,43 +1,46 @@
 <?php
 # 2024-01-29 "Refactor `config.inc.php` for phpPgAdmin": https://github.com/dmitrii-fediuk/5.9.188.84/issues/57
-$conf['servers'] = (function() {
-	$ss = [
-		'mage2.ru' => 14578
-		,'discourse-forum.ru' => 14579
-		,'mage2.pro' => 14580
-		,'discourse.pro' => 14581
-		,'df.tips' => 14582
-		,'oplatform.club' => 14583
-		,'maian.family' => 14584
-		,'rc.plus' => 14585
-		,'dmitry.ai' => 14587
-		,'rpa.how' => 14588
-	];
-	return array_map(function(string $desc, int $port):array {return [
-		'defaultdb' => 'discourse'
-		,'desc' => $desc
-		,'host' => 'localhost'
-		,'pg_dump_path' => '/usr/bin/pg_dump'
-		,'pg_dumpall_path' => '/usr/bin/pg_dumpall'
-		,'port' => $port
-		,'sslmode' => 'allow'
-	]; }, array_keys($ss), $ss);
-})();
-$conf['default_lang'] = 'auto';
-$conf['autocomplete'] = 'default on';
-$conf['extra_login_security'] = false;
-$conf['owned_only'] = false;
-$conf['show_comments'] = true;
-$conf['show_advanced'] = false;
-$conf['show_system'] = false;
-$conf['min_password_length'] = 1;
-$conf['left_width'] = 200;
-$conf['theme'] = 'default';
-$conf['show_oids'] = false;
-$conf['max_rows'] = 30;
-$conf['max_chars'] = 50;
-$conf['use_xhtml_strict'] = false;
-$conf['help_base'] = 'http://www.postgresql.org/docs/%s/interactive/';
-$conf['ajax_refresh'] = 3;
-$conf['plugins'] = [];
-$conf['version'] = 19;
+$conf = [
+	'servers' => (function() {
+		$ss = [
+			'mage2.ru' => 14578
+			,'discourse-forum.ru' => 14579
+			,'mage2.pro' => 14580
+			,'discourse.pro' => 14581
+			,'df.tips' => 14582
+			,'oplatform.club' => 14583
+			,'maian.family' => 14584
+			,'rc.plus' => 14585
+			,'dmitry.ai' => 14587
+			,'rpa.how' => 14588
+		];
+		return array_map(function(string $desc, int $port):array {return [
+			'defaultdb' => 'discourse'
+			,'desc' => $desc
+			,'host' => 'localhost'
+			,'pg_dump_path' => '/usr/bin/pg_dump'
+			,'pg_dumpall_path' => '/usr/bin/pg_dumpall'
+			,'port' => $port
+			,'sslmode' => 'allow'
+		]; }, array_keys($ss), $ss);
+	})()
+] + [
+	'ajax_refresh' => 3
+	,'autocomplete' => 'default on'
+	,'default_lang' => 'auto'
+	,'extra_login_security' => false
+	,'help_base' => 'http://www.postgresql.org/docs/%s/interactive/'
+	,'left_width' => 200
+	,'max_chars' => 50
+	,'max_rows' => 30
+	,'min_password_length' => 1
+	,'owned_only' => false
+	,'plugins' => []
+	,'show_advanced' => false
+	,'show_comments' => true
+	,'show_oids' => false
+	,'show_system' => false
+	,'theme' => 'default'
+	,'use_xhtml_strict' => false
+	,'version' => 19
+] + (isset($conf) ? $conf : []);
